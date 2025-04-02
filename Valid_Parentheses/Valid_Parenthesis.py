db0418a7d54472194a577148a1b3df17ee4d9e41
ludersryan <ggmates2043@gmail.com>
2025-04-01 20:48:07 -0700

    Solved valid parentheses

---
+++ Valid_Parentheses/Valid_Parenthesis.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")" : "()", "]" : "[", "{" : "}"}
+        
+        for c in s:
+            if c in closeToOpen: # a closing bracket
+                if stack and stack[-1] == closeToOpen[c]: # must both be closing
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c) # add to stack if its a open 
+        return True if not stack else False # stack must be empty for true
