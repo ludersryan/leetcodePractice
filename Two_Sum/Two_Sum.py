3bed101c9b221af5eb97bed84bbe87e8f3f14d57
ludersryan <ggmates2043@gmail.com>
2025-04-02 08:14:44 -0700

    Solved Roman To Integer

--- Two_Sum/Two_Sum.py
+++ Two_Sum/Two_Sum.py
@@ -5,11 +5,9 @@ def two_sum(nums: List[int], target: int) -> List[int]:
     myDict = {}
     for i, num in enumerate(nums):
         complement = target - num
-        if complement in myDict.values():
-            return [complement, num]
-        myDict[i] = num
-
-
+        if complement in myDict.keys():
+            return [myDict[complement], i]
+        myDict[num] = i
     return []
 
 
